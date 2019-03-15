19d36a7e2b448c8a983353b4c8ea416c08763b9a
Robert-Paul Respicio <respicio.robertpaul@gmail.com>
2019-03-14 18:24:12 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <div className="content">
+          <h1 className="title">How's the weather today?</h1>
+          <div className="input-group mb-5">
+            <input type="text" className="form-control" placeholder="Enter your zipcode" />
+            <div className="input-group-append">
+              <button className="btn btn-info" type="button">Let's find out!</button>
+            </div>
+          </div>
+          <div className="currentWrapper">
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
