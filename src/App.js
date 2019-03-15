e035091322cee38432eafb9ef6585ebabd2335af
Robert-Paul Respicio <respicio.robertpaul@gmail.com>
2019-03-14 20:10:33 -0700

    3rd commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipcode: '',
+    }
+  }
+
+  onChange = e => {
+    let zipcode = e.target.value;
+    this.setState({
+      zipcode
+    })
+    console.log(this.state.zipcode);
+  }
+
+  onButtonClick = () => {
+    console.log("Button clicked");
+  }
+
   render() {
     return (
       <React.Fragment>
         <div className="content">
           <h1 className="title">How's the weather today?</h1>
           <div className="input-group mb-5">
-            <input type="text" className="form-control" placeholder="Enter your zipcode" />
+            <input type="text" className="form-control" placeholder="Enter your zipcode" onChange={this.onChange} value={this.state.value} />
             <div className="input-group-append">
-              <button className="btn btn-info" type="button">Let's find out!</button>
+              <button className="btn btn-info" type="button" onClick={this.onButtonClick}><i class="fas fa-cloud-sun"></i> Let's find out!</button>
             </div>
           </div>
           <div className="currentWrapper">
